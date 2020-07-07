930bc0fb20d8c6d71921246cafa9182702e45108
RyosukeKamimura <come1234560@gmail.com>
2020-07-07 15:19:38 +0900

    Initial Commit

---
+++ CoreDataAndSwiftUI/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  CoreDataAndSwiftUI
+//
+//  Created by 神村亮佑 on 2020/07/07.
+//  Copyright © 2020 神村亮佑. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
---
+++ CoreDataAndSwiftUI/User+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  User+CoreDataProperties.swift
+//  CoreDataAndSwiftUI
+//
+//  Created by 神村亮佑 on 2020/07/07.
+//  Copyright © 2020 神村亮佑. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var admin: Bool
+    @NSManaged public var userId: String?
+    @NSManaged public var nickname: String?
+    @NSManaged public var password: String?
+
+}
